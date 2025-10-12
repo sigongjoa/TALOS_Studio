94654ac5b6ca474b34a1aa3bca1c5426392e1c8f
sigongjoa <zeskywa499@gmail.com>
2025-10-12 22:53:19 +0900

    Fix(CI): Dynamically set HISTORY_JSON_PATH in script

--- .github/scripts/generate_history.sh
+++ .github/scripts/generate_history.sh
@@ -18,7 +18,13 @@ else
   # Running in master branch context, .history is checked out
   if [ -d ".history" ]; then
   # Running in master branch context, .history is checked out
+  if [ -d ".history" ]; then
+  # Running in master branch context, .history is checked out
   HISTORY_JSON_PATH=".history/docs/manga_distribution_research/deployment_history.json"
+else
+  # Running in gh-pages branch context
+  HISTORY_JSON_PATH="docs/manga_distribution_research/deployment_history.json"
+fi
 else
   # Running in gh-pages branch context
   HISTORY_JSON_PATH="docs/manga_distribution_research/deployment_history.json"
